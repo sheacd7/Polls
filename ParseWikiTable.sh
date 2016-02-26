c3b154ba733d4206adba0ec0c4be1209698bbda5
sheacd7 <cds5g@virginia.edu>
2016-02-26 16:11:59 -0500

    add default output name based on input name

--- ParseWikiTable.sh
+++ ParseWikiTable.sh
@@ -6,8 +6,10 @@
 # Author: Colin Shea
 # Created: 2015-12-28
 # TODO:
-# trim leading whitespace, trailing ','
+#   trim leading whitespace, trailing ','
 
+# DONE
+#   use filename from input for default output name
 
 
 scriptname=$(basename $0)
@@ -32,15 +34,24 @@ while [[ $# > 0 ]]; do
   shift
 done
 
+# 
+if [[ -z "${OUT_FILE}" ]]; then
+  OUT_FILE="${IN_FILE%.*}.csv"
+fi
+
 # first grab each wikitable table into separate array
 echo "Reading from file: ${IN_FILE}"
 mapfile -t content < "${IN_FILE}"
 # get line numbers of table open/close tags
 echo "Parsing tables"
 mapfile -t table_o < \
-  <( printf '%s\n' "${content[@]}" | grep -n '<table class="wikitable"' | cut -d: -f1 )
+  <( printf '%s\n' "${content[@]}" | \
+     grep -n '<table class="wikitable"' | \
+     cut -d: -f1 )
 mapfile -t table_c < \
-  <( printf '%s\n' "${content[@]}" | grep -n '</table>'                 | cut -d: -f1 )
+  <( printf '%s\n' "${content[@]}" | \
+     grep -n '</table>' | \
+      cut -d: -f1 )
 
 i=0
 # for each table
@@ -56,9 +67,9 @@ for line_o in "${table_o[@]}"; do
   # replace td and /td with ','
   # replace tr and /tr with '\n'
   printf '%s' "${table[@]}" | \
-    sed -e 's, id=,,g;s, href=,,g;s, class=,,g;s, style=,,g;s, title=,,g;s, rowspan=,,g' \
+    sed -e 's, id=,,g;s, href=,,g;s, class=,,g;s, style=,,g;s, title=,,g;s, rowspan=,,g;s, rel=,,g' \
         -e 's,"[^"]*",,g;s,&#160;?,,g' \
-        -e 's,</*a>,,g;s,</*b>,,g;s,</*sup>,,g;s,</*span>,,g;s,</*small>,,g;s,</*abbr>,,g' \
+        -e 's,</*a>,,g;s,</*b>,,g;s,</*p>,,g;s,</*sup>,,g;s,</*span>,,g;s,</*small>,,g;s,</*abbr>,,g' \
         -e 's,</*th>,,g;s,<br\ />,,g;s,<td>,,g;s,</td>,\,,g;s,<tr>,,g;s,</tr>,\n,g' >> "${OUT_FILE}"
 
 done 
